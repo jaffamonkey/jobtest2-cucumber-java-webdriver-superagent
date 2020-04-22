022d889b5b7cca3d79c21ef8f6092a61d3c4ec7f
jaffamonkey <paullittlebury@mail.com>
2020-04-22 23:44:21 +0100

    Updated API test

--- tests/api/api-test-superagent-assert.js
+++ tests/api/api-test-superagent-assert.js
@@ -1,7 +1,7 @@
 const superagent = require('superagent');
 const assert = require('assert');
 
-// Task 1
+// Backend Task 1
 superagent
   .get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map')
   .query({ symbol: 'BTC,USDT,ETH' })
@@ -16,7 +16,7 @@ superagent
     assert.equal(res.status, 200);
   });
 
-// Task 1 Conversion 1
+// Backend Task 1 Conversion 1
 superagent
   .get('https://pro-api.coinmarketcap.com/v1/tools/price-conversion')
   .query({ id: 1, amount: 50, convert: 'BOB' })
@@ -24,13 +24,13 @@ superagent
   .set('Accept', 'application/json')
   .set('Accept', 'application/gzip')
   .end((err, res) => {
-    console.log('BOB Price conversion :' + res.body.data.quote.BOB.price)
+    console.log('BTC -> BOB Price conversion :' + res.body.data.quote.BOB.price)
     assert.ifError(err);
     assert.equal(res.status, 200);
     assert.equal(res.body.data.symbol, 'BTC');
   });
 
-// Task 1 Conversion 2
+// Backend Task 1 Conversion 2
 superagent
   .get('https://pro-api.coinmarketcap.com/v1/tools/price-conversion')
   .query({ id: 1027, amount: 50, convert: 'BOB' })
@@ -38,13 +38,13 @@ superagent
   .set('Accept', 'application/json')
   .set('Accept', 'application/gzip')
   .end((err, res) => {
-    console.log('BOB Price conversion :' + res.body.data.quote.BOB.price)
+    console.log('ETH -> BOB Price conversion :' + res.body.data.quote.BOB.price)
     assert.ifError(err);
     assert.equal(res.status, 200);
     assert.equal(res.body.data.symbol, 'ETH');
   });
 
-// Task 1 Conversion 3
+// Backend Task 1 Conversion 3
 superagent
   .get('https://pro-api.coinmarketcap.com/v1/tools/price-conversion')
   .query({ id: 825, amount: 50, convert: 'BOB' })
@@ -52,13 +52,13 @@ superagent
   .set('Accept', 'application/json')
   .set('Accept', 'application/gzip')
   .end((err, res) => {
-    console.log('BOB Price conversion :' + res.body.data.quote.BOB.price)
+    console.log('USDT -> BOB Price conversion :' + res.body.data.quote.BOB.price)
     assert.ifError(err);
     assert.equal(res.status, 200);
     assert.equal(res.body.data.symbol, 'USDT');
   });
 
-// Task 2
+// Backend Task 2
 superagent
   .get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/info')
   .query({ id: '1027' })
@@ -75,4 +75,21 @@ superagent
     assert.equal(res.body.data['1027'].symbol, 'ETH');
     assert.equal(res.body.data['1027'].logo, 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png');
     assert.equal(res.body.data['1027'].urls.technical_doc, 'https://github.com/ethereum/wiki/wiki/White-Paper');
+  });
+
+// Backend Optional Task
+superagent
+  .get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/info')
+  .query({ id: '1,2,3,4,5,6,7,8,9,10' })
+  .set('X-CMC_PRO_API_KEY', 'c46a40fe-3036-471d-97de-f0b277938fea')
+  .set('Accept', 'application/json')
+  .set('Accept', 'application/gzip')
+  .end((err, res) => {
+    for (i = 1; i < 11; i++) {
+      checkTags = res.body.data[i].tags
+      if (checkTags !== null) {
+        // will only print out symbols of those with tags
+        console.log(res.body.data[i].symbol);
+      }
+    }
   });
